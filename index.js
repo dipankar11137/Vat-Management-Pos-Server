ce84483c68d2194c03909526c7d50b3996d8283c
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-24 09:51:58 +0600

    get buy

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     const bookProductCollection = client
       .db('vat_management')
       .collection('bookings');
+    const buyProductCollection = client.db('vat_management').collection('buys');
 
     //   // // // // // // // // // // // //
 
@@ -119,144 +120,19 @@ async function run() {
       const result = await bookProductCollection.deleteOne(query);
       res.send(result);
     });
-    // // // get product filter by category
-    // app.get('/products/:category', async (req, res) => {
-    //   const category = req.params.category;
-    //   const query = { category };
-    //   const cursor = productCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-    // // get product by id
-    // app.get('/product/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await productCollection.findOne(query);
-    //   res.send(result);
-    // });
-    // // // restock Product item and update
-    // app.put('/productId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateQuantity = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       quantity: updateQuantity.quantity,
-    //     },
-    //   };
-    //   const result = await productCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // // Delete one product
-    // app.delete('/product/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await productCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-
-    // //                     Buy    //
-    // // post buy
-    // app.post('/buyProducts', async (req, res) => {
-    //   const postResult = req.body;
-    //   const result = await buyProductCollection.insertOne(postResult);
-    //   res.send(result);
-    // });
-    // // // get buy products
-    // app.get('/buyProducts', async (req, res) => {
-    //   const query = {};
-    //   const cursor = buyProductCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-    // // get by product by id
-    // app.get('/buyProductId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await buyProductCollection.findOne(query);
-    //   res.send(result);
-    // });
-    // // // get buy filter by email
-    // app.get('/buyProduct/:email', async (req, res) => {
-    //   const email = req.params.email;
-    //   const query = { email };
-    //   const cursor = buyProductCollection.find(query);
-    //   const user = await cursor.toArray();
-    //   res.send(user);
-    // });
-    // // //  buy product update payment
-    // app.put('/buyProductPayment/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatePayment = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       payment: updatePayment.payment,
-    //     },
-    //   };
-    //   const result = await buyProductCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // product buy update delivered
-    // app.put('/buyDelivered/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateDelivered = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       delivered: updateDelivered.delivered,
-    //     },
-    //   };
-    //   const result = await buyProductCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-
-    // // Delete Buy Product
-    // app.delete('/buyProduct/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await buyProductCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-
-    // //                       contact     //
-    // // post contact
-    // app.post('/contact', async (req, res) => {
-    //   const postResult = req.body;
-    //   const result = await contactCollection.insertOne(postResult);
-    //   res.send(result);
-    // });
-
-    // // Get all contact
-    // app.get('/contact', async (req, res) => {
-    //   const query = {};
-    //   const cursor = contactCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
-    // // Delete Buy contact
-    // app.delete('/contact/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await contactCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    // buy information
+    app.post('/buys', async (req, res) => {
+      const postResult = req.body;
+      const result = await buyProductCollection.insertOne(postResult);
+      res.send(result);
+    });
+    // // get booking products
+    app.get('/buys', async (req, res) => {
+      const query = {};
+      const cursor = buyProductCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
